5b22c122f55f577ee7b60f4425b9e2228d98ce2f
Charles Mpon A Dang <154475579+charly-fullstack@users.noreply.github.com>
2025-12-26 01:53:52 +0200

    Add first Java program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("My name is Charly");
+        System.out.println("I am learning Java step by step");
+    }
+
+}
