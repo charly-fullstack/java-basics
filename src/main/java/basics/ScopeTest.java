f808d81e248cdd359eb8418d6a42f3aa1a8bac1e
charles <charles@example.com>
2026-01-05 01:54:48 +0200

    Exercice 1 - portée locale vs variable de classe

---
+++ src/main/java/basics/ScopeTest.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class ScopeTest {
+
+    static String message = "Bonjour";
+
+    public static void main(String[] args) {
+        afficher();
+        System.out.println(message);
+
+    }
+
+    static void afficher() {
+        String nom = "Jean";
+        System.out.println(message);
+        System.out.println(message + nom);
+    }
+}
