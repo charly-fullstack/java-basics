ce0ff409ab2772108f74d5be942ca0bc1b92c8ca
charles <charles@example.com>
2026-01-15 03:03:17 +0200

    Exercice Java – boucles

---
+++ src/main/java/basics/TableDeMultiplication.java
@@ -0,0 +1,22 @@
+package basics;
+
+import java.util.Scanner;
+
+public class TableDeMultiplication {
+
+    public static void main(String[] args) {
+        TableDeMultiplication t = new TableDeMultiplication();
+        t.afficherTableDeMultiplication();
+    }
+
+    void afficherTableDeMultiplication() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Entrez un nombre : ");
+        int nombre = scanner.nextInt();
+//        scanner.close(); // facultatif ici, car System.in
+        for (int i = 1; i <= 10; i++ ) {
+            System.out.println(nombre + " * " + i + " = " + (nombre * i));
+        }
+    }
+}
